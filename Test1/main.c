793a71c3a0d72b2ef73a5c97370b7491f2f26833
Oriane Viceconte <o.viceconte@ludus-academie.com>
2020-11-12 20:11:00 +0100

    add commit -m 'Next_intro'

--- Test1/main.c
+++ Test1/main.c
@@ -4,25 +4,48 @@
 int main()
 {
     // exemple
-    int n_a2=0, n_b2=0;
-    char c_car2='a';
+    int n_a=0, n_b=0;
+    char c_car='a';
     float flt_f='0.0';
     float flt_test;
     int n_text;
 
     printf("Bonjour\n");
     printf("Programme exemple\n");
-//Affichage formaté
-    printf("n_a : %d\n",n_a2);
-    printf("n_b : %d\n",n_b2);
-    printf("c_car2 : %c\n",c_car2);
+//Affichage formate
+    printf("n_a : %d\n",n_a);
+    printf("n_b : %d\n",n_b);
+    printf("c_car : %c\n",c_car);
     printf("flt_f : %f\n",flt_f);
     printf("flt_test : %d\n",flt_test);
 
+    //Affichage non formate
+    putchar(c_car);
+
+    /* printf("\nEntrer un entier : \n");
+    scanf("%d,&n_a);
+    printf("Entrer un entier :\n);
+    scanf("%d",&n_b); */
+
+    printf("\nEntrer deux entiers : \n");
+    scanf("%d%d",&n_a,n_b);
+
+    printf("n_a : %d\n",n_a);
+    scanf("n_b : %d\n",n_b);
+
+    fflush(stdin); //vider le tampon stdin
+    printf("Entrer un caractere : \n");
+    scanf("%c",&c_car);
+
+    printf("c_car : %c",c_car);
     //il faut toujours initialiser en C !
 
-    prinf("Entrer un entier\n");
-    scan("%d",&n-test);
+    fflush(stdin); //vider le tampon stdin
+    printf("Entrer un caractere : \n");
+    c_car=getchar();
+
+    printf("c_car : %c",c_car);
+
 
     //
 
