5b58f31f643e4188178ef9d6bef08f6c7f682907
Oriane Viceconte <o.viceconte@ludus-academie.com>
2020-11-12 17:04:52 +0100

    add commit -m'Next'

--- Test1/main.c
+++ Test1/main.c
@@ -3,86 +3,31 @@
 
 int main()
 {
-    short sn_entier;
-    int n_entier;
-    long ln_entier;
+    // exemple
+    int n_a2=0, n_b2=0;
+    char c_car2='a';
+    float flt_f='0.0';
+    float flt_test;
+    int n_text;
 
-    printf("taille en octet du short : %d\n",sizeof(sn_entier));
-    printf("taille en octet du int : %d\n",sizeof(n_entier));
-    printf("taille en octet du long : %d\n",sizeof(ln_entier));
+    printf("Bonjour\n");
+    printf("Programme exemple\n");
+//Affichage formaté
+    printf("n_a : %d\n",n_a2);
+    printf("n_b : %d\n",n_b2);
+    printf("c_car2 : %c\n",c_car2);
+    printf("flt_f : %f\n",flt_f);
+    printf("flt_test : %d\n",flt_test);
 
-    //reel
-    //unsigned : non signée ex :unsigned float flt_reel
+    //il faut toujours initialiser en C !
 
-    float flt_reel;
-    double dbl_reel;
-    long double ldbl_reel;
+    prinf("Entrer un entier\n");
+    scan("%d",&n-test);
 
-    printf("taille en octet du float : %d\n",sizeof(flt_reel));
-    printf("taille en octet du double : %d\n",sizeof(dbl_reel));
-    printf("taille en octet du long double : %d\n",sizeof(ldbl_reel));
+    //
 
-    //caractere
-
-    char c_car;
-    printf("taille en octet du caractere : %d\n",sizeof(c_car));
-
-    //operateurs
-
-  //  int n_a=0;
-  //  int n_b=0;
-
-  //  printf("%d",(a==b);
-    //retourne la valeur 1 (trus) ou 0 (false)
-
-    //type d'egalite
-
-    //== teste d'egalite
-    //<=
-    //>=
-    //le différent n'existe pas en c
-    // il s'agit du not egal : != (pas egal)
-
-    //operateur d'indexation
-    //+
-    //-
-    //*
-    /* '/' */ //-> operateur de division, retourne la partie entière (pour entier)
-    // -> il faut utiliser des reels pour le DIV
-    // % -> MODULO
-    // = -> assignation
-    //, -> indexation / separateur de sequences
-
-    //operateurs logiques
-    // & (et logque) -> &&
-    // | (ou logique) -> || optimise la sequence de test
-    // ! (not)
-
-    // operateurs rapide
-    // ++ -> incrémentation -> +1
-    // --
-    // +=
-    // -=
-    // /=
-    // *=
-
-    // \n -> saut de ligne
-    // \t -> tabulation
-
-    int n_a=0;
-    int n_b=0;
-
-    printf("val de n_a : %d et nb_b : %d \n",n_a,n_b);
-    printf("val de n_a : %x et nb_b : %x \n",n_a,n_b);
-//%d -> placement de l'entier
-
-// printf -> ECRIRE
-// scanf() -> LIRE
-
-    printf("Saisir un entier dans n_a \n");
-    scanf("%d",&n_a);
-    printf("Saisir un entier dans n_b \n");
-    scanf("%d",&n_b);
+    //bien ppuyer sur entrer après chaque entrer de nombre
+    //sinon il les conciderera comme un seul.
 
 
     return 0;
