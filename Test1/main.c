9c2a3c068abe7f5638869c59543038e5b465e67c
Oriane Viceconte <o.viceconte@ludus-academie.com>
2020-11-12 19:54:15 +0100

    add commit -m 'Next_intro'

--- Test1/main.c
+++ Test1/main.c
@@ -1,33 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NBI 10
 
 int main()
 {
-    // exemple
-    int n_a2=0, n_b2=0;
-    char c_car2='a';
-    float flt_f='0.0';
-    float flt_test;
-    int n_text;
+    //Instructions de controle
+    //SI -> if
+    int n_a=0,n_b=0;
 
-    printf("Bonjour\n");
-    printf("Programme exemple\n");
-//Affichage formaté
-    printf("n_a : %d\n",n_a2);
-    printf("n_b : %d\n",n_b2);
-    printf("c_car2 : %c\n",c_car2);
-    printf("flt_f : %f\n",flt_f);
-    printf("flt_test : %d\n",flt_test);
+    //Condition
+    if(n_a==n_b){
+        printf("n_a et n_b identiques\n");
+    }
 
-    //il faut toujours initialiser en C !
+    // SINON -> else
 
-    prinf("Entrer un entier\n");
-    scan("%d",&n-test);
+        if(n_a==n_b){
+        printf("n_a et n_b identiques\n");
+    }
+    else{
+        printf("n_a et n_b pas identiques\n");
+    }
+    //autres exemples
+    if(n_a==n_b){
+        printf("n_a et n_b identiques\n");
+    }
+    else if(n_a>n_b){
+            //Instructions
+        printf("n_a > n_b\n");
+    }
+    else{
+        //instructions
+        printf("n_a et n_b differents\n");
+    }
 
-    //
+    int n_val=12;
+    if(n_val>=10 && n_val<=20) {
+        //instructions
+        printf("%d", n_val);
+    }
 
-    //bien ppuyer sur entrer après chaque entrer de nombre
-    //sinon il les conciderera comme un seul.
+    // CAS PARMI -> switch case
+    int n_choix=0;
+
+    printf("Menu\n");
+    printf("1:Menu 1\n");
+    printf("2:Menu 2\n");
+    printf("Choix ?\n");
+    scanf("%c",&n_choix);
+
+    switch (n_choix) {
+        case '1' : printf("Menu 1\n");break;
+        case '2' : printf("Menu 2\n");break;
+    }
+
+    // CAS PAR DEFAUT -> default ajoute a la fin
+
+    int n_choix2=0;
+
+    printf("Menu\n");
+    printf("1:Menu 1\n");
+    printf("2:Menu 2\n");
+    printf("Choix ?\n");
+    scanf("%d",&n_choix2);
+
+    switch (n_choix2) {
+        case '1' : printf("Menu 1\n");break;
+        case '2' : printf("Menu 2\n");break;
+        default:printf("defaut\n");
+    }
+
+    // Boucle POUR
+    int n_i;
+    for(n_i=0;n_i<NBI;n_i++) {
+            printf("%d\n",n_i);
+    }
+
+        // FAIRE TANT QUE
+        n_i=0;
+        do{
+            printf("%d\n",n_i);
+            n_i++;
+        }while(n_i<NBI); //quand on est oblige de le faire au moins une fois
+
+        //TANT QUE -> tant que l'on ne connait pas le nombre de fois à faire
+        while(n_i<NBI){
+            printf("%d\n",n_i);
+            n_i++;
+        }
+
 
 
     return 0;
